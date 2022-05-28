601dc5f6be6b5129786a925d09aca1e382a2282b
Mattz <matias.garcia.r@hotmail.es>
2022-05-28 12:21:27 -0300

    cargue archivos

---
+++ aprendiendo programacion.cs
@@ -0,0 +1,67 @@
+
+        /*
+        Console.WriteLine ("Ingresa el valor entero de A:");
+        int a =  Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine ("Ingresa el valor entero de B:");
+        int b =  Convert.ToInt32(Console.ReadLine());
+        int suma = a + b;
+        Console.WriteLine ("El resultado de a+b es "+suma);
+
+        Console.WriteLine ("Ingresa su nombre:");
+        String nombre = Console.ReadLine();
+        Console.WriteLine ("Hola "+nombre+", alias amante de Mocchi.");
+
+        Console.WriteLine ("Ingresa el valor entero de A: ");
+        int a1 = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine ("Ingresa el valor entero de B: ");
+        int b1 = Convert.ToInt32(Console.ReadLine());
+        int producto = a1 * b1;
+        Console.WriteLine ("El resultado de a*b es "+producto);
+
+        int a = 33; 
+        int b = 4;
+        int div = a/b;
+        int mod = a%b;
+        Console.WriteLine ("div: "+div + " mod: "+mod);
+
+        bool a = 15 > 30;
+        bool b = 15 > 30;
+        bool c = 15 == 30;
+        bool d = 15 != 30;
+        bool e = !a;
+        Console.WriteLine ("a "+a+"\nb "+b+"\nc "+c+"\nd "+d+"\ne "+e);
+
+        Console.WriteLine ("Ingresa el valor entero de A: ");
+        int a1 = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine ("Ingresa el valor entero de B: ");
+        int b1 = Convert.ToInt32(Console.ReadLine());
+        if(a1 >= b1){
+            Console.WriteLine ("El valor de A es mayor o igual al valor de B");
+        } else{
+            Console.WriteLine ("El valor de A es menor al valor de B");
+        }
+          
+using System; || Ejercicio 5
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Cantidad de horas trabajadas:");
+        int hts = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine ("Valor en $(pesos argentinos) por hora trabajada:");
+        double pph = Convert.ToDouble(Console.ReadLine());
+        double SAL;
+        if(hts > 35){
+            int extra = hts - 35;
+            double prod = extra * (pph * 1.5);
+            SAL = prod + (pph * 35);
+        } else{
+            SAL = hts * pph;
+        }
+        Console.WriteLine ("Usted percibira $"+SAL+" por sus horas brindadas");
+    }
+}
+        */
+
+
